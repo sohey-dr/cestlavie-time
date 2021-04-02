e341e6af82adce4dce5bc9cd072890bfd018264b
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-02 12:24:11 +0900

    Add: RailsAdminの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'toppages#index'
   get 'second', to: 'toppages#second'
   resources :bands, only: [:update]
