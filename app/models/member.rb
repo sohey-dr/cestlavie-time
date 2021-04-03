c1af65627e15db7b7ba0e2730c6c59bbd1599118
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-03 22:25:55 +0900

    Modify: Enumerize設定

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,5 +3,5 @@ class Member < ApplicationRecord
   has_many :bands, through: :band_members
 
   extend Enumerize
-  enumerize :member_type, in: { "" 1, aupay: 2, choice: 3, furunavi: 4, furusato_premium: 5, rakuten: 6, ana: 7 }
+  enumerize :member_type, in: { "サークルメンバー" => 1, "OB" => 2, "他サークル" => 3 }
 end
