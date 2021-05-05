9331c5b5bcc7bd0b065a0d4125e48a12ae12f117
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-05-05 20:50:14 +0900

    Modify: 普通のページのbasic認証解除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  # before_action :basic_auth
 
   private
 
   def basic_auth
-    # authenticate_or_request_with_http_basic do |username, password|
-    #   username == '' && password == ''
-    # end
+    authenticate_or_request_with_http_basic do |username, password|
+      username == '' && password == ''
+    end
   end
 end
