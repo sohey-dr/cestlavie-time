a3895601eb429faf27218eefe8fb536d9abc2b99
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-05-05 21:03:29 +0900

    Config: sidekiqにbasic認証追加

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,10 @@
+require 'sidekiq'
+require 'sidekiq/web'
+
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  [user, password] == [ENV['BASIC_USER'], ENV['BASIC_PASS']]
+end
+
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://localhost:6379', namespace: 'test_sidekiq' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  require 'sidekiq/web'
-  mount Sidekiq::Web, at: "/sidekiq"
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'  
+  Rails.application.routes.draw do
+    mount Sidekiq::Web, at: "/sidekiq"
+  end
 
   root to: 'toppages#index'
   get 'second', to: 'toppages#second'
