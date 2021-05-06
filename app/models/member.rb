0f17c193d5b7b6e414511c80cde27c2f1a274918
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-05-07 02:47:58 +0900

    Add: passwordカラム追加

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,7 @@
 class Member < ApplicationRecord
   has_many :band_members
   has_many :bands, through: :band_members
+  has_secure_password
 
   extend Enumerize
   enumerize :member_type, in: { "サークルメンバー" => 1, "OB" => 2, "他サークル" => 3 }
