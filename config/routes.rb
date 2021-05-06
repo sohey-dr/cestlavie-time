8fec47a3ed9c24631a53b4e53fbb70f98515aa28
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-05-07 02:23:06 +0900

    Config: membersのapiのroute設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
     mount Sidekiq::Web, at: "/sidekiq"
   end
 
+  namespace 'api' do
+    namespace 'v1' do
+      resources :members, :only => :show
+    end
+  end
+
   root to: 'toppages#index'
   get 'second', to: 'toppages#second'
   resources :bands, only: [:update]
